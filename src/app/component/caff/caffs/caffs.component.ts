ebaa127f06e90b7c1ff30aaa878042c5a75fc06d
horlor <horvath.lorant@edu.bme.hu>
2021-11-23 11:44:10 +0100

    Implementing list view

--- src/app/component/caff/caffs/caffs.component.ts
+++ src/app/component/caff/caffs/caffs.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap, RoutesRecognized } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { CaffService } from 'src/app/services/caff.service';
 import { AnimationResponse } from 'src/app/shared/models/animation.model';
 
 @Component({
   selector: 'app-caffs',
   templateUrl: './caffs.component.html',
-  styleUrls: ['./caffs.component.css']
+  styleUrls: ['./caffs.component.css'],
+  providers:[CaffService]
 })
 export class CaffsComponent implements OnInit {
-
+  loading = false;
   caffs: Array<AnimationResponse> = [];
-  constructor() { }
+  constructor(    
+    private router: Router,
+    private route: ActivatedRoute,
+    private caffService: CaffService) 
+    { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.loading = true
+    const snapshot = this.route.snapshot;
+    let search = snapshot.queryParamMap.get("search") || ""
+    let page = parseInt(snapshot.queryParamMap.get("page") ||"0") || 0
+    const res = await this.caffService.GetAnimations(search,page).toPromise();
+    this.caffs = res.content
+    this.loading = false
   }
 
   
--- src/app/services/caff.service.ts
+++ src/app/services/caff.service.ts
@@ -1,11 +1,33 @@
+import { query } from '@angular/animations';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AnimationDetailedResponse, AnimationListResponse } from '../shared/models/animation.model';
 
-const AUTH_API = 'http://localhost:8081/api/anim/';
+const ANIM_API = '/api/anim';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class CaffService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public GetAnimations(search:string="",page:number=1,pageSize=20, sorted=false): Observable<AnimationListResponse>{
+      const params = new URLSearchParams();
+      if(search)
+       params.set("query",search);
+      params.set("pageNumber",page.toFixed(0));
+      params.set("pageSize",pageSize.toFixed(0))
+      return this.http.get<AnimationListResponse>(ANIM_API+"?"+params.toString(),httpOptions);
+  }
+
+  public GetAnimationDetail(id:string){
+    return this.http.get<AnimationDetailedResponse>(ANIM_API+"/"+id,httpOptions);
+  }
+
 }
